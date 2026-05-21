f0f0469a59a3465e5a1a83fda870dd28c2c55e57
Manish Joshi <mjoshi2233@gmail.com>
2026-05-21 11:31:52 +0530

    Implement merge sort algorithm in C++

---
+++ merge_sort.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+
+void merge(int arr[], int low, int mid, int high)
+{
+    int temp[100];
+    int i = low;
+    int j = mid + 1;
+    int k = low;
+
+    while(i <= mid && j <= high)
+    {
+        if(arr[i] < arr[j])
+            temp[k++] = arr[i++];
+        else
+            temp[k++] = arr[j++];
+    }
+
+    while(i <= mid)
+        temp[k++] = arr[i++];
+
+    while(j <= high)
+        temp[k++] = arr[j++];
+
+    for(int i = low; i <= high; i++)
+        arr[i] = temp[i];
+}
+
+void mergeSort(int arr[], int low, int high)
+{
+    if(low < high)
+    {
+        int mid = (low + high) / 2;
+
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+
+        merge(arr, low, mid, high);
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    mergeSort(arr, 0, n - 1);
+
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
